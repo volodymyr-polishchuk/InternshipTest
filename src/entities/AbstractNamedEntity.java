09781f994ff11052174ae560694195bcbdf69559
polishchuk <thevivalley@gmail.com>
2019-01-28 21:47:33 +0200

    Create AbstractNamedEntity as Skeleton of NamedEntity

---
+++ src/entities/AbstractNamedEntity.java
@@ -0,0 +1,19 @@
+package entities;
+
+public abstract class AbstractNamedEntity implements NamedEntity {
+    private String name;
+
+    public AbstractNamedEntity(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+}
