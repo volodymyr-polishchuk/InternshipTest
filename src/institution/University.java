696d5677b8fde9f7a80d9623a13098e503ba804d
polishchuk <thevivalley@gmail.com>
2019-01-28 21:51:48 +0200

    Implement all method for University

--- src/institution/University.java
+++ src/institution/University.java
@@ -1,17 +1,31 @@
 package institution;
 
+import entities.AbstractInstitutionEntity;
 import person.Student;
 
-public class University {
+public class University extends AbstractInstitutionEntity {
+    private double averageBuffer = 0;
+    private boolean averageBufferActuality = true;
+
     public University(String name) {
-        //TODO: Implementation is needed
+        super(name);
     }
 
-    public void setStudent(Student student) {
-        //TODO: Implementation is needed
+    public double getAverageStudentKnowledge() {
+        if (!averageBufferActuality) {
+            averageBuffer = students.stream()
+                    .mapToDouble(i -> i.getKnowledge().getLevel())
+                    .average()
+                    .orElse(Double.NaN);
+            averageBufferActuality = true;
+        }
+        return averageBuffer;
     }
 
+    @Override
     public void addStudent(Student student) {
-        //TODO: Implementation is needed
+        super.addStudent(student);
+        averageBufferActuality = false;
     }
+
 }
