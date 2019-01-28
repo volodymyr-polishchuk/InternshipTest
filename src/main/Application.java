6113d8f79a93cc9c02d6dcefe14a98406e255666
polishchuk <thevivalley@gmail.com>
2019-01-28 21:54:33 +0200

    Create application that work

--- src/main/Application.java
+++ src/main/Application.java
@@ -1,18 +1,26 @@
 package main;
 
+import dao.HardCodeStudentDAO;
+import dao.StudentDAO;
 import institution.University;
 import institution.interlink.Internship;
 import person.Student;
 
+import java.util.List;
+
 public class Application {
+    private static StudentDAO studentDAO = new HardCodeStudentDAO();
+
     public static void main(String[] args) {
+        List<Student> students = studentDAO.getStudents();
+
         University university = new University("CH.U.I.");
-        university.addStudent(new Student("Andrew Kostenko"));
-        university.addStudent(new Student("Julia Veselkina"));
-        university.addStudent(new Student("Maria Perechrest"));
+        students.forEach(university::addStudent);
 
         Internship internship = new Internship("Interlink");
+        university.getStudents().forEach(student -> internship.addStudent(student, university));
+
         System.out.println("List of internship's students:");
-        System.out.println(internship.getStudents());
+        System.out.println(internship.getStudentsNames());
     }
 }
