f6027e863163847a12d5ed268a559a8982291f59
polishchuk <thevivalley@gmail.com>
2019-01-28 21:49:38 +0200

    Create AbstractInstitutionEntity that extend AbstractNamedEntity

---
+++ src/entities/AbstractInstitutionEntity.java
@@ -0,0 +1,43 @@
+package entities;
+
+import person.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class AbstractInstitutionEntity extends AbstractNamedEntity {
+    protected List<Student> students = new ArrayList<>();
+
+    public AbstractInstitutionEntity(String name) {
+        super(name);
+    }
+
+    protected void addStudent(Student student) {
+        students.add(student);
+    }
+
+    protected void setStudents(List<Student> students) {
+        this.students = new ArrayList<>(students);
+    }
+
+    public int getStudentCount() {
+        return students.size();
+    }
+
+    public Student getStudent(int index) {
+        return students.get(index);
+    }
+
+    public List<Student> getStudents() {
+        return new ArrayList<>(students);
+    }
+
+    public String getStudentsNames() {
+        final StringBuilder builder = new StringBuilder(students.size() * 15);
+        students.forEach(student -> {
+            builder.append(student.toString()).append('\n');
+        });
+        return builder.toString();
+    }
+
+}
